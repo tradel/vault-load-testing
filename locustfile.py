8a18be9f0a2b0fdd446b9093b682df73ea349331
Todd Radel <todd@hashicorp.com>
2018-03-26 08:04:22 -0400

    Add debug logging, uncomment as necessary

--- locustfile.py
+++ locustfile.py
@@ -1,3 +1,15 @@
 from locusts.key_value import *
 from locusts.transit import *
 from locusts.pki import *
+from locusts.dyn_mongodb import *
+from locusts.dyn_mysql import *
+from locusts.totp import *
+
+# import logging
+# logging.getLogger().setLevel(logging.DEBUG)
+# requests_log = logging.getLogger("requests.packages.urllib3")
+# requests_log.setLevel(logging.DEBUG)
+# requests_log.propagate = True
+#
+# import http.client
+# http.client.HTTPConnection.debuglevel = 1
