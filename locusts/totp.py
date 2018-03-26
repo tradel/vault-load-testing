819108f89b2c0c70f10a2789d3edc490a9f877f5
Todd Radel <todd@hashicorp.com>
2018-03-26 08:05:55 -0400

    Add TOTP code generation

---
+++ locusts/totp.py
@@ -0,0 +1,37 @@
+import json
+import sys
+
+import os
+import random
+
+from locust import HttpLocust, task
+
+sys.path.append(os.getcwd())
+import common
+
+from locusts import VaultTaskSet, VaultLocust
+
+
+class TotpTasks(VaultTaskSet):
+    KEY_NAME = 'test-totp-key'
+    ISSUER = 'Vault'
+    ACCOUNT_NAME = 'test@test.com'
+
+    def setup(self):
+        self.mount('totp')
+        if not self.is_in_list(self.KEY_NAME, '/v1/totp/keys'):
+            self.client.post(f'/v1/totp/keys/{self.KEY_NAME}',
+                             json={'generate': True,
+                                   'issuer': self.ISSUER,
+                                   'account_name': self.ACCOUNT_NAME})
+
+    @task
+    def generate(self):
+        r = self.client.get(f'/v1/totp/code/{self.KEY_NAME}')
+
+
+class TotpLocust(VaultLocust):
+    task_set = TotpTasks
+    weight = 1
+    min_wait = 5000
+    max_wait = 60000
